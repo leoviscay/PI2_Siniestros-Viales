63780fb76a38639915e7be9df516ca8ff49e428c
Leonel Viscay <leonelviscay@gmail.com>
2024-01-03 01:10:34 -0300

    Subo archivos de data, y EDA de Pestaña Hechos

---
+++ jupyter/utils.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from datetime import datetime
+
+
+def types_data_df(df):
+    '''
+    Analiza de los tipos de datos en un DataFrame y devuelve un resumen que incluye información sobre
+    los tipos de datos en cada columna, el porcentaje de valores no nulos y nulos, así como la
+    cantidad de valores nulos por columna.
+
+    Parameters:
+        df (pandas.DataFrame): El DataFrame que se va a analizar.
+
+    Returns:
+        pandas.DataFrame: Un DataFrame que contiene el resumen de cada columna, incluyendo:
+        - 'nombre_campo': Nombre de cada columna.
+        - 'tipo_datos': Tipos de datos únicos presentes en cada columna.
+        - 'no_nulos_%': Porcentaje de valores no nulos en cada columna.
+        - 'nulos_%': Porcentaje de valores nulos en cada columna.
+        - 'nulos': Cantidad de valores nulos en cada columna.
+    '''
+
+    mi_dict = {"nombre_campo": [], "tipo_datos": [], "no_nulos_%": [], "nulos_%": [], "nulos": []}
+
+    for columna in df.columns:
+        porcentaje_no_nulos = (df[columna].count() / len(df)) * 100
+        mi_dict["nombre_campo"].append(columna)
+        mi_dict["tipo_datos"].append(df[columna].apply(type).unique())
+        mi_dict["no_nulos_%"].append(round(porcentaje_no_nulos, 2))
+        mi_dict["nulos_%"].append(round(100-porcentaje_no_nulos, 2))
+        mi_dict["nulos"].append(df[columna].isnull().sum())
+
+    df_info = pd.DataFrame(mi_dict)
+        
+    return df_info
+
+def convert_to_datatime(x):
+    '''
+    Convierte un valor a un objeto de tiempo (time) de Python si es posible.
+
+    Esta función acepta diferentes tipos de entrada y trata de convertirlos en objetos de tiempo (time) de Python.
+    Si la conversión no es posible, devuelve None.
+
+    Parameters:
+        x (str, datetime, or any): El valor que se desea convertir a un objeto de tiempo (time).
+
+    Returns:
+        datetime.time or None: Un objeto de tiempo (time) de Python si la conversión es exitosa,
+        o None si no es posible realizar la conversión.
+    '''
+    if isinstance(x, str):
+        try:
+            return datetime.strptime(x, "%H:%M:%S").time()
+        except ValueError:
+            return None
+    elif isinstance(x, datetime):
+        return x.time()
+    return x
